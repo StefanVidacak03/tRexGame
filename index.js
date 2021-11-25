c3384c507bad5a9dfa1fe0370c42ac050007ff16
Stefan0333 <49637187+Stefan0333@users.noreply.github.com>
2021-11-25 23:20:30 +0100

    Commit 1
    Kreiran pocetni "index.html" fajl

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/views"+ "/"+"index.html");
    })
+
+   const path = require('path')
+app.use('/static', express.static(path.join(__dirname, '/public' +'/'+'css'+'styles.css')))
+
 app.listen(3000)
